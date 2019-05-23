61688b9098ac92d5c4edddeb9a1b36658ffe49dd
Julien Bloit <julien.bloit@gmail.com>
2019-05-23 15:17:38 +0200

    more packages and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,48 @@ module.exports = {
         useLangKeyLayout: true,
         prefixDefault: true,
       }
-    }],
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+    },
+    {
+      // a "source" plugin: bring data into gatsby's data system
+
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+
+    // a "transformer" plugin: transform raw data brought by source plugins
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 600,
+            },
+          },
+        ],
+      },
+    },
+
+
+    // Image 
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-remark-copy-linked-files`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/img/`,
+      },
+    },
+
+
+
+  ],
 }
