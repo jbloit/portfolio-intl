95e7efb6473a70512b0a3bd64295923b0ffe82f5
Julien Bloit <julien.bloit@gmail.com>
2019-09-06 13:28:07 +0200

    better pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,7 +58,21 @@ module.exports = {
       },
     },
 
-
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Julien Bloit`,
+        short_name: `J. Bloit`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/img/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
 
   ],
 }
