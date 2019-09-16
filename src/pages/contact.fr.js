0aaca71c16976a51000cda621c0d41808c866f39
Julien Bloit <julien.bloit@gmail.com>
2019-09-16 12:06:59 +0200

    social buttons

--- src/pages/contact.fr.js
+++ src/pages/contact.fr.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import Layout from "../layouts/layout"
+import Img from "gatsby-image";
+import { FaTwitter, FaGithub, FaEnvelope, FaLinkedin } from 'react-icons/fa';
+
+// see https://react-icons.netlify.com/#/icons/fa
+
 
 const IndexPage = (props) => (
   <Layout location={props.location}>
-  <div><p>J'accepte de nouvelles collaborations</p></div>
+  <div><p>J'accepte de nouvelles collaborations </p></div>
         
+
+<div className="social">
+<div><a href="https://github.com/jbloit" target="_blank"><FaGithub size="2em"/></a></div>
+<div><a href="https://www.linkedin.com/in/julien-bloit-4081b16/" target="_blank"><FaLinkedin size="2em"/></a></div>
+<div><a href="https://twitter.com/jbloit" target="_blank"><FaTwitter size="2em"/></a> </div>
+
+
+<div><a href="mailto:julien.bloit@gmail.com"><FaEnvelope size="2em"/></a> </div>
+</div>
+
   </Layout>
 )
 
 export default IndexPage
+
