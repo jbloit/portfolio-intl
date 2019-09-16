57da6eafb94a7ff648d3d97f9595b06e039c5e5f
Julien Bloit <julien.bloit@gmail.com>
2019-09-16 12:59:41 +0200

    100 height

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,9 @@ import Layout from "../layouts/layout"
 const IndexPage = (props) => (
   <Layout location={props.location}>
             <br/>
-      
+      <p>
             Je construis des dispositifs pour modeler la matière musicale, à des fins artistiques, pédagogiques ou récréatives.
-     
+     </p>
 
   </Layout>
 )
