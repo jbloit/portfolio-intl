89e53b55d9a50f56ced7ce60868e1814cf281074
Julien Bloit <julien.bloit@gmail.com>
2019-09-16 14:52:29 +0200

    image in about

--- src/pages/about.fr.js
+++ src/pages/about.fr.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Layout from "../layouts/layout"
+import myFace from "../img/about/me_2019_square.png"
 
 const IndexPage = (props) => (
   <Layout location={props.location}>
     
     
-    <div className="plain">
-    
+    <div className="about">
     <p> 
+  <img src={myFace} alt="Me"/>
+
+   
     Je sais faire 
     <ul>
       <li>
