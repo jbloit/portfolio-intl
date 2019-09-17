9353a3d6e7c4c08bd8b37de1c8420a693138c03a
Julien Bloit <julien.bloit@gmail.com>
2019-09-17 17:01:23 +0200

    about version 1

--- src/pages/about.fr.js
+++ src/pages/about.fr.js
@@ -4,31 +4,58 @@ import Layout from "../layouts/layout"
 import myFace from "../img/about/me_2019_square.png"
 import { FaTwitter, FaGithub, FaEnvelope, FaLinkedin } from 'react-icons/fa';
 
-const IndexPage = (props) => (
-  <Layout location={props.location}>
 
-    <div className="plain">
-      <div className="about">
+const IndexPage = (props) => {
 
+  return (
+    <Layout location={props.location}>
 
-        {/* <p>The tomato (see pronunciation) is the edible, often red berry-type fruit of the nightshade Solanum lycopersicum, commonly known as a tomato plant. The tomato is consumed in diverse ways, including raw, as an ingredient in many dishes, sauces, salads, and drinks.</p> */}
-
-        <img src={myFace} alt="Me" />
+      <div className="plain">
+        <div className="about">
 
 
-        <p>The tomato (see pronunciation) is the edible, often red berry-type fruit of the nightshade Solanum lycopersicum, commonly known as a tomato plant. The tomato is consumed in diverse ways, including raw, as an ingredient in many dishes, sauces, salads, and drinks.</p>
+          {/* <p>The tomato (see pronunciation) is the edible, often red berry-type fruit of the nightshade Solanum lycopersicum, commonly known as a tomato plant. The tomato is consumed in diverse ways, including raw, as an ingredient in many dishes, sauces, salads, and drinks.</p> */}
 
-        <div className="social">
-        <div><a href="https://github.com/jbloit" target="_blank"><FaGithub size="2em" /></a></div>
-        <div><a href="https://www.linkedin.com/in/julien-bloit-4081b16/" target="_blank"><FaLinkedin size="2em" /></a></div>
-        <div><a href="https://twitter.com/jbloit" target="_blank"><FaTwitter size="2em" /></a> </div>
+          <img src={myFace} alt="Me" />
+
+
+          <p>Je suis développeur audio, spécialisé en applications musicales. Je travaille sur des supports tangibles - musées, installations - ou virtuels -applications mobiles.</p>
+
+          <p>J'ai travaillé pendant près de dix ans à l'<a href="https://www.ircam.fr" target="_blank" rel="noopener noreferrer">Ircam</a>, passant du développement web dans le studio hypermédia fondé par Bernard Stiegler, à la recherche en intéraction musicale temps réel. J'y ai obtenu un doctorat, dont la thèse portait sur l'apprentissage machine appliqué à la transcription musicale.</p>
+
+          <p> Je suis un travailleur indépendant basé à Bruxelles. J'ai de l'expérience aussi bien avec les <Link to="fr/petiteFabrique">institutions Françaises</Link>, les <Link to="fr/humtap">startups Californiennes</Link> et les <Link to="fr/gd_sansTitre">artistes Marseillais</Link>. Je suis toujours prêt à <Link to="fr/contact">discuter de votre projet</Link> si vous pensez que je pourrais vous aider.</p>
+
+        <p>Particulièrement sensible aux applications pédagogiques, j'anime un <a href="https://www.spoken.brussels/robotics" target="_blank" rel="noopener noreferrer">atelier hebdomadaire de robotique pour enfants.</a></p>
+
+
+  
+         <div className="skills">
+            <div className="domain">Développment Mobile </div> <div className="skillset">Swift, Objective-C, Kotlin, CoreAudio, AudioKit, Link API</div>
+            <div className="domain">Lutherie numérique / sound design </div> <div className="skillset">Max/MSP, SuperCollider, Ableton Live, Reaper, Audacity, Bela, audio sur RaspberryPi, Jack</div>
+            <div className="domain">Langages génériques </div> <div className="skillset">Python, C++, Java</div>
+            <div className="domain">Méthodes agiles</div> <div className="skillset">Scrum, Kanban, Jira, Trello</div>
+            <div className="domain">Musique</div> <div className="skillset">Batteur +30ans d'expérience, Harmonie 4 voies.</div>
+         </div>
+
+         {/* <h3>Science</h3>
+         <div className="skills">
+            <div className="domain">Machine Learning</div> <div className="skillset">Hidden Markov Models, Viterbi decoding</div>
+            <div className="domain">Langages génériques </div> <div className="skillset">Python, C++, Java</div>
+         </div> */}
+
+
+          <div className="social">
+            <div><a href="https://github.com/jbloit" target="_blank"><FaGithub size="2em" /></a></div>
+            <div><a href="https://www.linkedin.com/in/julien-bloit-4081b16/" target="_blank"><FaLinkedin size="2em" /></a></div>
+            <div><a href="https://twitter.com/jbloit" target="_blank"><FaTwitter size="2em" /></a> </div>
+          </div>
+
+        </div>
+
       </div>
 
-      </div>
-
-    </div>
-
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default IndexPage
