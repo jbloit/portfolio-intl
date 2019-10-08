1167c8ccafc8bd11f15dbb14706984a2ac1ffd91
Julien Bloit <julien.bloit@gmail.com>
2019-10-08 16:38:51 +0200

    fix analytics?

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,11 +78,11 @@ module.exports = {
       options: {
         trackingId: "UA-149619817-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
-        respectDNT: true,
+        respectDNT: false,
         // Avoids sending pageview hits from custom paths
         // exclude: ["/preview/**", "/do-not-track/me/too/"],
         // Delays sending pageview hits on route update (in milliseconds)
